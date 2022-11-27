ce29fd58de1c15fea3d995c2237bfd5e36e67800
Cort <1944792+cdwfs@users.noreply.github.com>
2022-11-26 23:17:09 -0800

    Customize template and rebuild all days

--- src/day02.zig
+++ src/day02.zig
@@ -1,17 +1,91 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
 const util = @import("util.zig");
-const gpa = util.gpa;
-
 const data = @embedFile("../data/day02.txt");
 
+const Input = struct {
+    allocator: std.mem.Allocator,
+
+    pub fn init(input_text: []const u8, allocator: std.mem.Allocator) !@This() {
+        var lines = std.mem.tokenize(u8, input_text, "\r\n");
+        _ = lines;
+        var input = Input{
+            .allocator = allocator,
+        };
+        errdefer input.deinit();
+
+        return input;
+    }
+    pub fn deinit(self: @This()) void {
+        _ = self;
+    }
+};
+
+fn part1(input: Input) i64 {
+    _ = input;
+    return 0;
+}
+
+fn part2(input: Input) i64 {
+    _ = input;
+    return 0;
+}
+
+const test_data =
+    \\test data here
+;
+const part1_test_solution: ?i64 = null;
+const part1_solution: ?i64 = null;
+const part2_test_solution: ?i64 = null;
+const part2_solution: ?i64 = null;
+
+// Just boilerplate below here, nothing to see
+
+fn testPart1(allocator: std.mem.Allocator) !void {
+    var test_input = try Input.init(test_data, allocator);
+    defer test_input.deinit();
+    if (part1_test_solution) |solution| {
+        try std.testing.expectEqual(solution, part1(test_input));
+    }
+
+    var timer = try std.time.Timer.start();
+    var input = try Input.init(data, allocator);
+    defer input.deinit();
+    if (part1_solution) |solution| {
+        try std.testing.expectEqual(solution, part1(input));
+        print("part1 took {d:9.3}ms\n", .{@intToFloat(f64, timer.lap()) / 1000000.0});
+    }
+}
+
+fn testPart2(allocator: std.mem.Allocator) !void {
+    var test_input = try Input.init(test_data, allocator);
+    defer test_input.deinit();
+    if (part2_test_solution) |solution| {
+        try std.testing.expectEqual(solution, part2(test_input));
+    }
+
+    var timer = try std.time.Timer.start();
+    var input = try Input.init(data, allocator);
+    defer input.deinit();
+    if (part2_solution) |solution| {
+        try std.testing.expectEqual(solution, part2(input));
+        print("part2 took {d:9.3}ms\n", .{@intToFloat(f64, timer.lap()) / 1000000.0});
+    }
+}
+
 pub fn main() !void {
-    
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    try testPart1(allocator);
+    try testPart2(allocator);
+}
+
+test "part1" {
+    try testPart1(std.testing.allocator);
+}
+
+test "part2" {
+    try testPart2(std.testing.allocator);
 }
 
 // Useful stdlib functions
@@ -36,6 +110,7 @@ const max = std.math.max;
 const max3 = std.math.max3;
 
 const print = std.debug.print;
+const expect = std.testing.expect;
 const assert = std.debug.assert;
 
 const sort = std.sort.sort;
