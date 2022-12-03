5e245778d3b79a38005b76034cb294588b72da2d
Cort <1944792+cdwfs@users.noreply.github.com>
2022-12-03 13:04:42 -0800

    day 2 solutions

--- README.md
+++ README.md
@@ -46,4 +46,9 @@ A list of the puzzles, and what new language/tool features I learned each day:
   - append `catch unreachable` to an error union to say "this can never fail, just give me the value".
 - `std.sort.sort(i64, array, {}, comptime std.sort.desc(i64))` to sort a slice in descending order.
 
-
+### [Day 2: Rock Paper Scissors](https://adventofcode.com/2022/day/2)
+- Cobweb-clearing
+- Don't try to guess what part 2 will be while solving part 1! (I speculatively over-generalized my part1 solution, and guessed wrong)
+- Enums look like structs. You can give them an ordinal type, and access it with `@enumToInt(MyEnum.Value)`.
+- You can't switch on tuples. I feel like that would've worked in Rust.
+- I _almost_ had a use case for initializing a lookup table using a comptime function, but it was just too easy to do in my head & hard-code the values instead. Maybe next time.
--- src/day02.zig
+++ src/day02.zig
@@ -2,16 +2,26 @@ const std = @import("std");
 const util = @import("util.zig");
 const data = @embedFile("data/day02.txt");
 
+const Round = struct {
+    opponent_move_code: u8,
+    my_move_code: u8,
+};
+
 const Input = struct {
     allocator: std.mem.Allocator,
+    rounds: std.BoundedArray(Round, 2500),
 
     pub fn init(input_text: []const u8, allocator: std.mem.Allocator) !@This() {
         var lines = std.mem.tokenize(u8, input_text, "\r\n");
-        _ = lines;
         var input = Input{
             .allocator = allocator,
+            .rounds = try std.BoundedArray(Round, 2500).init(0),
         };
         errdefer input.deinit();
+
+        while (lines.next()) |line| {
+            input.rounds.appendAssumeCapacity(Round{ .opponent_move_code = line[0] - 'A', .my_move_code = line[2] - 'X' });
+        }
 
         return input;
     }
@@ -20,23 +30,57 @@ const Input = struct {
     }
 };
 
+const Move = enum(u4) {
+    R,
+    P,
+    S,
+};
+
+const move_scores: [3]i64 = .{ 1, 2, 3 };
+const result_scores: [9]i64 = .{
+    3, 6, 0, // R/R, R/P, R/S
+    0, 3, 6, // P/R, P,P, P/S
+    6, 0, 3, // S/R, S/P, S/S
+};
 fn part1(input: Input) !i64 {
-    _ = input;
-    return 0;
+    var total_score: i64 = 0;
+    const opponent_move_map: [3]Move = .{ Move.R, Move.P, Move.S };
+    const my_move_map: [3]Move = .{ Move.R, Move.P, Move.S };
+    for (input.rounds.constSlice()) |round| {
+        const opponent_move = opponent_move_map[round.opponent_move_code];
+        const my_move = my_move_map[round.my_move_code];
+        total_score += move_scores[@enumToInt(my_move)] + result_scores[@enumToInt(opponent_move) * 3 + @enumToInt(my_move)];
+    }
+    return total_score;
 }
 
+const move_for_result: [9]Move = .{
+    Move.S, Move.R, Move.P, // R/L, R/D, R/W
+    Move.R, Move.P, Move.S, // P/L, P,D, P/W
+    Move.P, Move.S, Move.R, // S/L, S/D, S/W
+};
+
 fn part2(input: Input) !i64 {
-    _ = input;
-    return 0;
+    var total_score: i64 = 0;
+    const opponent_move_map: [3]Move = .{ Move.R, Move.P, Move.S };
+    for (input.rounds.constSlice()) |round| {
+        const opponent_move = opponent_move_map[round.opponent_move_code];
+        const desired_result = round.my_move_code;
+        const my_move = move_for_result[@enumToInt(opponent_move) * 3 + desired_result];
+        total_score += move_scores[@enumToInt(my_move)] + result_scores[@enumToInt(opponent_move) * 3 + @enumToInt(my_move)];
+    }
+    return total_score;
 }
 
 const test_data =
-    \\test data here
+    \\A Y
+    \\B X
+    \\C Z
 ;
-const part1_test_solution: ?i64 = null;
-const part1_solution: ?i64 = null;
-const part2_test_solution: ?i64 = null;
-const part2_solution: ?i64 = null;
+const part1_test_solution: ?i64 = 15;
+const part1_solution: ?i64 = 11873;
+const part2_test_solution: ?i64 = 12;
+const part2_solution: ?i64 = 12014;
 
 // Just boilerplate below here, nothing to see
 
