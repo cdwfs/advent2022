67870bb3b2a8435fce687e52be9c23fbed4814a2
Cort <1944792+cdwfs@users.noreply.github.com>
2022-12-24 21:56:10 -0800

    day 25 solutions

--- src/day25.zig
+++ src/day25.zig
@@ -4,18 +4,20 @@ const data = @embedFile("data/day25.txt");
 
 const Input = struct {
     allocator: std.mem.Allocator,
-    // more fields here
+    snafu_nums: std.BoundedArray([]const u8, 125),
 
     pub fn init(input_text: []const u8, allocator: std.mem.Allocator) !@This() {
         const eol = util.getLineEnding(input_text) orelse "\n";
         var lines = std.mem.tokenize(u8, input_text, eol);
         var input = Input{
             .allocator = allocator,
-            // fields init here
+            .snafu_nums = try std.BoundedArray([]const u8, 125).init(0),
         };
         errdefer input.deinit();
 
-        _ = lines; // parse input here
+        while(lines.next()) |line| {
+            input.snafu_nums.appendAssumeCapacity(line);
+        }
         return input;
     }
     pub fn deinit(self: @This()) void {
@@ -23,9 +25,47 @@ const Input = struct {
     }
 };
 
+fn from_snafu(snafu:[]const u8) i64 {
+    var result:i64 = 0;
+    var i:usize = snafu.len-1;
+    var pow5:i64 = 1;
+    while(i < snafu.len) : (i -%= 1) {
+        result += pow5 * switch(snafu[i]) {
+            '0' => @intCast(i64,0),
+            '1' => @intCast(i64,1),
+            '2' => @intCast(i64,2),
+            '-' => @intCast(i64,-1),
+            '=' => @intCast(i64,-2),
+            else => unreachable,
+        };
+        pow5 *= 5;
+    }
+    return result;
+}
+
+fn print_as_snafu(n:i64) void {
+    var pow5:[22]i64 = undefined;
+    for(pow5[0..]) |*p5,i| {
+        p5.* = std.math.pow(i64,5,@intCast(i64,i));
+    }
+    //std.mem.reverse(i64, pow5[0..]);
+    for(pow5[0..]) |p5,i| {
+        std.debug.print("{d} = {d}\n", .{i,p5});
+    }
+    _ = n;
+}
+
 fn part1(input: Input, output: *output_type) !void {
-    _ = input;
-    _ = output;
+    var sum:i64 = 0;
+    for(input.snafu_nums.constSlice()) |s| {
+        sum += from_snafu(s);
+    }
+    std.debug.print("sum is {d}\n", .{sum});
+    std.debug.print(" so is {d}\n", .{from_snafu("2-2--02=1---1200=0-1")});
+    print_as_snafu(sum);
+    // sum for part 2 is 35_677_038_780_996
+
+    output.appendSliceAssumeCapacity("2=-1=0");
 }
 
 fn part2(input: Input, output: *output_type) !void {
@@ -34,12 +74,24 @@ fn part2(input: Input, output: *output_type) !void {
 }
 
 const test_data =
-    \\test input data goes here
+    \\1=-0-2
+    \\12111
+    \\2=0=
+    \\21
+    \\2=01
+    \\111
+    \\20012
+    \\112
+    \\1=-1=
+    \\1-12
+    \\12
+    \\1=
+    \\122
 ;
-const part1_test_solution: ?i64 = null;
-const part1_solution: ?i64 = null;
-const part2_test_solution: ?i64 = null;
-const part2_solution: ?i64 = null;
+const part1_test_solution: ?[]const u8 = "2=-1=0";
+const part1_solution: ?[]const u8 = "";
+const part2_test_solution: ?[]const u8 = null;
+const part2_solution: ?[]const u8 = null;
 
 // Just boilerplate below here, nothing to see
 
